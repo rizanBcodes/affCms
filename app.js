1744b0e3b99cd5b31100cb0ee71ca9db30e6ae07
rizanBcodes <100106906+rizanBcodes@users.noreply.github.com>
2022-05-02 00:05:36 +0545

    initialized repo- working db & show posts

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const port = 4000;
+const connection = require('./database.js')
+const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set(
+    'view engine',
+    'ejs'
+)
+
+app.use(express.static('public'));
+
+connection.connect();
+
+
+app.listen(port);
+
+app.post('/addpost', urlencodedParser, (req, res) => {
+    console.log('backend got an api hit at addpost');
+    console.log('starting query')
+    connection.query(`insert into blogs(slug, title, description) values (?, ?, ?)`,
+        [req.body.postSlug, req.body.postTitle, req.body.postDescr],
+        (error, results) => {
+            if (error) console.log(error);
+            console.log('inserted these values into test database')
+            console.log(req.body.postSlug);
+            console.log(req.body.postTitle);
+            console.log(req.body.postDescr);
+            console.log('ending connection');
+        }
+    )
+
+})
+
+app.get(
+    '/about',
+    (req, res) => {
+        console.log('inside about route')
+
+        res.render('about',
+            {
+                title: 'About Affcms'
+            })
+    }
+)
+
+app.get(
+    '/contact',
+    (req, res) => {
+        console.log('inside contact route')
+
+        res.render('contact',
+            {
+                title: 'Contact'
+            })
+    }
+)
+
+app.get(
+    '/privacy-policy',
+    (req, res) => {
+        res.render('privacy-policy',
+            {
+                title: 'Privacy'
+            })
+    }
+)
+
+app.get(
+    '/addpost',
+    (req, res) => {
+        console.log('inside add post route');
+        res.render('addpost');
+    }
+)
+
+
+app.get(
+    '/',
+    (req, res) => {
+        connection.query(`select * from blogs`,
+            (error, results) => {
+                if (error) console.log(error);
+                console.log('inside root route');
+                res.render(
+                    'index',
+                    {
+                        title: 'Home page',
+                        blogs: results
+                    }
+                )
+            }
+        )
+
+    }
+)
+
+app.get(
+    '/:slug',
+    (req, res) => {
+        console.log('inside :slug route');
+        slug = req.params.slug;
+        connection.query(`select * from blogs where slug='${slug}'`,
+            (error, results) => {
+                if (error) console.log(error);
+                res.render(
+                    'details',
+                    {
+                        title: results[0].title,
+                        description: results[0].description
+                    }
+                )
+            }
+        )
+    }
+)
+
---
+++ public/addpost.js
@@ -0,0 +1,95 @@
+// CKEDITOR.replace('postDescr');
+
+// let addPost = document.getElementById('addPost'); 
+let shoutTitleStatus = document.getElementById('shoutTitleStatus');
+let shoutDescriptionStatus = document.getElementById('shoutDescriptionStatus');
+
+
+let shoutStatus = document.getElementById('shoutStatus');
+let form = document.getElementById('form');
+
+function clearForm() {
+    postTitle.value = '';
+    clearInvalidTitleMessage();
+    postDescr.value = '';
+    clearInvalidDescriptionMessage();
+}
+
+function postSubmitted() {
+    shoutStatus.textContent = 'Post submitted successfully!';
+}
+
+function shoutInvalidTitle() {
+    shoutTitleStatus.textContent = 'title cannot be blank. Please enter valid inputs.';
+}
+
+function shoutInvalidDescription() {
+    shoutDescriptionStatus.textContent = 'description cannot be blank';
+}
+
+function clearInvalidTitleMessage(){
+    shoutTitleStatus.textContent = '';
+}
+
+function clearInvalidDescriptionMessage(){
+    shoutDescriptionStatus.textContent = '';
+}
+
+function submitForm() {
+    form.submit();
+    console.log('sent post request to backend');
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let postTitle = document.getElementById('postTitle').value;
+    let postDescr = document.getElementById('postDescr').value;
+
+    if (postTitle == '' && postDescr == '') {
+        shoutInvalidTitle();
+        shoutInvalidDescription();
+    } else if (postTitle == '' && postDescr !== '' ) {
+        shoutInvalidTitle();
+        clearInvalidDescriptionMessage();
+    } else if (postDescr == '' && postTitle !== '') {
+        shoutInvalidDescription();
+        clearInvalidTitleMessage();
+    } else {
+        submitForm();
+        clearForm();
+        postSubmitted();
+    }
+
+    // if(postTitle!== '' && postDescr !== ''){
+    //     submitForm();
+    //     clearForm();
+    //     postSubmitted();
+    //     console.log('form cleared');
+    // } else{
+    //     shoutInvalid();
+    // }
+})
+
+let testingLink = document.getElementById('testingLink');
+let testingTag = document.getElementById('testingTag');
+let affTagMaker = document.getElementById('affTagMaker');
+let affTag = document.getElementById('affTag');
+
+affTagMaker.addEventListener(
+    'click',
+    () => {
+        console.log('clickeeedd');
+        if (testingLink.value !== '' && testingTag.value !== '') {
+            let affLink = testingLink.value + '?tag=' + testingTag.value;
+            affTag.textContent = 'Link: ' + affLink;
+            navigator.clipboard.writeText(affLink);
+            affTagStatus.textContent = 'Copied to clipboard!';
+            testingLink.value = ''; //clear link field
+            testingTag.value = testingTag.value; //clear tag field
+        } else {
+            affTagStatus.textContent = 'please enter valid links/tag';
+            affTag.textContent = '';
+        }
+    }
+)
+
